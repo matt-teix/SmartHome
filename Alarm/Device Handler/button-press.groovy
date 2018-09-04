35d7b0a2d9a60d96fef07514b93fcbf87a67002c
Matt-Teix <re.lax@shaw.ca>
2018-09-03 19:08:45 -0600

    Revert "Revert "Alarm Device Manager""
    This reverts commit 7c6bc0abfe2813320ac7d4fb564f72277d5fde00.

---
+++ Alarm/Device Handler/button-press.groovy
@@ -0,0 +1,39 @@
+metadata {
+	definition (name: "Momentary Button Tile", namespace: "smartthings", author: "SmartThings") {
+		capability "Actuator"
+		capability "Switch"
+		capability "Momentary"
+		capability "Sensor"
+	}
+
+	// simulator metadata
+	simulator {
+	}
+
+	// UI tile definitions
+	tiles {
+		standardTile("switch", "device.switch", width: 2, height: 2, canChangeIcon: true) {
+			state "off", label: 'Push', action: "momentary.push", backgroundColor: "#ffffff", nextState: "on"
+			state "on", label: 'Push', action: "momentary.push", backgroundColor: "#53a7c0"
+		}
+		main "switch"
+		details "switch"
+	}
+}
+
+def parse(String description) {
+}
+
+def push() {
+	sendEvent(name: "switch", value: "on", isStateChange: true, displayed: false)
+	sendEvent(name: "switch", value: "off", isStateChange: true, displayed: false)
+	sendEvent(name: "momentary", value: "pushed", isStateChange: true)
+}
+
+def on() {
+	push()
+}
+
+def off() {
+	push()
+}
